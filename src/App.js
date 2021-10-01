06dfb5de58ac51dff5ce1bcf6bb79f68c5a62795
abelmontea <87101653+abelmontea@users.noreply.github.com>
2021-10-01 13:46:47 -1000

    Importing project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App container">
+      <form id="search-form">
+        <div id="search-engine" className="row justify-content-evenly">
+          <div className="col">
+            <input
+              type="text"
+              placeholder="Look up any city!"
+              className="search-city form-control"
+            />
+          </div>
+          <div className="col-md-auto">
+            <input
+              type="submit"
+              value="Search"
+              className="search-button btn btn-success"
+            />
+          </div>
+          <div className="col-md-auto">
+            <button className="use-current-location btn btn-primary">
+              Current Location
+            </button>
+          </div>
+        </div>
+      </form>
+      <div id="weather-info" className="border-white rounded">
+        <div id="city-state">Seattle Weather</div>
+        <div id="local-time">
+          As of <span id="day">Monday</span>, <span id="time">1:52 pm</span>
+        </div>
+        <div id="current-weather" className="row justify-content-between">
+          <div id="floating-temperature" className="col">
+            <span id="current-temperature">56</span>
+            <span id="units"> °F</span>
+            <div id="weather-description">
+              <h2>Clear</h2>
+            </div>
+          </div>
+          <div id="current-temperature-img" className="col-4">
+            <h2>
+              <span id="high-temp">85°</span>/<span id="low-temp"> 72°</span>
+            </h2>
+          </div>
+        </div>
+        <div id="weather-stats">
+          Humidity: <span id="humidity">2</span>%
+          <br />
+          Wind: <span id="wind-speed">5</span> m/s
+        </div>
+      </div>
+      <div className="github-link text-center">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/abelmontea/Working-Weather-App"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Open-source code
         </a>
-      </header>
+        , by Aisha Natividad
+      </div>
     </div>
   );
 }
